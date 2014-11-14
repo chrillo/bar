a5ecd6b1f518be64788889c8c9a9f8ed377f0112
Christoph Peter <peter@internetkultur.at>
2014-11-14 10:34:56 +0100

    removed log files

--- fuel/app/logs/2011/12/13.php
+++
@@ -1,4 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-Error - 2011-12-13 17:02:54 --> 2048 - mktime() [<a href='function.mktime'>function.mktime</a>]: You should be using the time() function instead in /Users/christophpeter/sites/bar/fuel/app/classes/controller/api.php on line 35
-Error - 2011-12-13 17:03:08 --> 2048 - mktime() [<a href='function.mktime'>function.mktime</a>]: You should be using the time() function instead in /Users/christophpeter/sites/bar/fuel/app/classes/controller/api.php on line 35
--- fuel/app/logs/2012/03/28.php
+++
@@ -1,7 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-Error - 2012-03-28 09:56:11 --> Parsing Error - syntax error, unexpected T_DOUBLE_ARROW in /Users/christophpeter/sites/bar/fuel/app/classes/controller/admin/items.php on line 62
-Error - 2012-03-28 09:56:50 --> Error - Call to undefined method Fuel\Core\Input::cost() in /Users/christophpeter/sites/bar/fuel/app/classes/controller/admin/items.php on line 33
-Error - 2012-03-28 09:57:02 --> Error - Call to undefined method Fuel\Core\Input::cost() in /Users/christophpeter/sites/bar/fuel/app/classes/controller/admin/items.php on line 33
-Error - 2012-03-28 10:25:11 --> 1060 - Duplicate column name 'cost' [ ALTER TABLE `items` ADD (
-	`cost` int(11) NOT NULL) ] in /Users/christophpeter/sites/bar/fuel/core/classes/database/mysql/connection.php on line 203
